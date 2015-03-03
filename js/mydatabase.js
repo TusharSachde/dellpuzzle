88cb6ad1f1814c8a82666416afbef81410b8127e
Abhay Amin <pixoloproductions@gmail.com>
2015-03-03 20:00:43 +0530

    database functions ready

--- js/mydatabase.js
+++ js/mydatabase.js
@@ -21,5 +21,43 @@ var mydatabase = angular.module('mydatabase', [])
     tx.executeSql('INSERT INTO USERS (id, name, email, phone) VALUES ('+data.id+', "'+data.name+'", "'+data.email+'", "'+data.phone+'")');
 });
             },
+            setmessage : function(data)
+            {
+                db.transaction(function (tx) {
+    tx.executeSql('UPDATE USERS SET `message`= "'+data+'" WHERE `id` ='+user.id);
+                });
+            },
+            setdots : function(data)
+            {
+                db.transaction(function (tx) {
+    tx.executeSql('UPDATE USERS SET `dots`= "'+data+'" WHERE `id` ='+user.id);
+                });
+            },
+            setjerseyscore : function(data)
+            {
+                db.transaction(function (tx) {
+    tx.executeSql('UPDATE USERS SET `jerseyscore`= "'+data+'" WHERE `id` ='+user.id);
+                });
+            },
+            settesttime : function(data)
+            {
+                db.transaction(function (tx) {
+    tx.executeSql('UPDATE USERS SET `testtime`= "'+data+'" WHERE `id` ='+user.id);
+                });
+            },
+            setcertificate : function(data)
+            {
+                db.transaction(function (tx) {
+    tx.executeSql('UPDATE USERS SET `certificate`= "'+data+'" WHERE `id` ='+user.id);
+                });
+            },
+            getmessage : function()
+            {
+                db.transaction(function (tx) {
+    tx.executeSql('SELECT `message` FROM USERS WHERE `id` ='+user.id, [], function(tx, results) {
+                        var message = results.rows.item(0).message;
+                    }, function(tx, results) {} );
+                });
+            },
         }
     });
